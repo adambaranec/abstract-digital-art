68f19743e85bba1c363260c48ff5b7ee12620c7a
Adam Baranec <35851756+adambaranec@users.noreply.github.com>
2023-04-10 08:55:27 +0200

    Update spiral_web.js

--- spiral_web/spiral_web.js
+++ spiral_web/spiral_web.js
@@ -18,14 +18,14 @@ function setup() {
 function draw() {
   background(red,green,blue);  
   if (frameCount % 60 == 0){seconds += 1;}
-  if (seconds == 120){seconds = 0;}
+  if (seconds == 121){seconds = 0;}
   if (seconds <= 30){
     densityX += (30/1800); densityY += (30/1800);
   }
-  else if (seconds > 30 || seconds <= 60){
+  else if (seconds > 30 && seconds != 61){
   fill(random(255), random(255), random(255));
   }
-  else if (seconds > 60 || seconds <= 120){
+  else if (seconds > 60 && seconds != 121){
    densityX -= (30/1800); densityY -= (30/1800);
   }
   if (minute() % 2 == 0){
